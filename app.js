5e2e996f179fd5cac10935388643ba316c4839a5
turnerriley <turnerriley@seattleacademy.org>
2015-05-19 14:56:42 -0700

    add sphere volume margin of error and roots of quadratic

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var geometry = require('./index.js');
-console.log( 'The area of a circle of radius 4 is '
-           + geometry.circlearea(4));
+// console.log( 'The area of a circle of radius 4 is '
+//            + geometry.circlearea(4));
 
-console.log( 'The circumfrence of a circle of radius 4 is ',
-           geometry.circlecircumfrence(4));
+// console.log( 'The circumfrence of a circle of radius 4 is ',
+//            geometry.circlecircumfrence(4));
 
-console.log( 'The area of a square with sides of 4 ',
-           geometry.squarearea(4));
+// console.log( 'The area of a square with sides of 4 ',
+//            geometry.squarearea(4));
+
+console.log( 'The roots of ax2 + bx + c are: ',
+           geometry.quadratic(1,-4,4));
+
+console.log( 'The volume of a sphere with radius 5 is: ',
+           geometry.spherevolume(5));
+
+console.log( 'The margin of error is: ',
+           geometry.marginerror(10,0.8));
